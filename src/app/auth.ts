a1a4649996cc26b3e045b76140f7413e4b97acd8
Anurag Dhote <anuragdhote392@gmail.com>
2024-04-14 12:42:18 +0530

    feat - backend auth complete

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,7 +1,62 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 import Google from "next-auth/providers/google";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Google]
-});
+import prisma from "@/lib/db";
 
+declare module "next-auth" {
+    interface Session {
+        user: {
+            userId: string
+        } & DefaultSession['user']
+    }
+}
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    providers: [Google],
+    callbacks: {
+        async signIn({ profile }) {
+            try {
+                if (!profile) {
+                    throw new Error('Profile doesn\'t exists');
+                }
+                // console.log({ profile });
+                const userExists = await prisma.user.findFirst({
+                    where: {
+                        email: profile.email || ""
+                    }
+                });
+                
+                if (!userExists) {
+                    await prisma.user.create({
+                        data: {
+                            email: profile.email || "",
+                            name: profile.name || ""
+                        }
+                    })
+                }
+
+                return true;
+            } catch (error) {
+                console.log({error});
+                return false;
+            }
+        },
+        async session({ session }) {
+            try {
+                if (!session) {
+                    throw new Error('Session doesn\'t exists');
+                }
+                const sessionUser = await prisma.user.findFirst({
+                    where: {
+                        email: session.user.email
+                    }
+                });
+                session.user.userId = sessionUser?.id || "";
+            } catch (error) {
+                console.log({error});
+            } finally {
+                return session;
+            }
+        }
+    }
+});
